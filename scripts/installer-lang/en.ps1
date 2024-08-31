3aef45ff06ca0e784a6ef776ada78c8d73de811a
chillmusic <37777162+chill-music@users.noreply.github.com>
2024-08-31 20:30:43 +0300

    Update en.ps1

--- scripts/installer-lang/en.ps1
+++ scripts/installer-lang/en.ps1
@@ -1,7 +1,7 @@
 [PSCustomObject]@{
     Welcome         = "
       ╔════════════════════════════════════════╗
-      ║ Welcome to Hurricane-SpotX for Windows ║
+      ║ Welcome to Hurricane-SpotX for Windows1 ║
       ╚════════════════════════════════════════╝"
     Incorrect       = "Oops, an incorrect value,"
     Incorrect2      = "enter again through "
