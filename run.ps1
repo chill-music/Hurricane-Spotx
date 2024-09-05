b2a77664ef56096370458afa61c7b7eef3035b4e
chillmusic <37777162+chill-music@users.noreply.github.com>
2024-09-06 02:14:57 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -299,8 +299,8 @@ function Get-Link {
     )
 
     switch ($mirror) {
-        $true { "https://hurricane-spotx-official.github.io/SpotX/scripts/installer-lang/$clg.ps1" }
-        default { "https://raw.githubusercontent.com/chill-music/Hurricane-Spotx/main/scripts/installer-lang/$clg.ps1" }
+        $true { return "https://hurricane-spotx-official.github.io/SpotX/scripts/installer-lang/$clg.ps1" + $endlink }
+        default { return "https://raw.githubusercontent.com/chill-music/Hurricane-Spotx/main/scripts/installer-lang/$clg.ps1" + $endlink }
     }
 }
 
